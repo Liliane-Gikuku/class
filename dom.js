8b39d1d82ce95396cd73789d630752e6e861a1d9
student <lilkundiro@gmail.com>
2022-08-15 17:24:27 +0300

    class

---
+++ dom.js
@@ -0,0 +1,20 @@
+// let enzo= document.createElement("div").
+// enzo.className="Enzo"
+// console.log(enzo);
+
+// let newChild=document.createElement("p")
+// .className="grandchild";
+// let text=document.createTextNode("This is a grandchild");
+// console.log({newChild});
+// newChild.appendChild(newChild)
+
+// let child=document.getElementsByClassName("container")[0].children
+// child[0].appendChild(enzo)
+// console.log(child[0].children);//add this "[0].children" to see how many children
+
+function e()
+{
+document.querySelector('#k').innerHTML=document.querySelector('#a').value
+}
+
+
